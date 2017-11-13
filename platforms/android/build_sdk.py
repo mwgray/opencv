f9451cc810f5d4a350ba06131aac66e252577ea5
Matthew Gray <mwgray@google.com>
2017-11-13 14:00:46 -0800

    updated versions

--- platforms/android/build_sdk.py
+++ platforms/android/build_sdk.py
@@ -76,13 +76,13 @@ class ABI:
         return self.name == "x86" or self.name == "x86_64"
 
 ABIs = [
-    ABI("2", "armeabi-v7a", "arm-linux-androideabi-4.8", cmake_name="armeabi-v7a with NEON"),
-    ABI("1", "armeabi",     "arm-linux-androideabi-4.8"),
+    ABI("2", "armeabi-v7a", "arm-linux-androideabi-4.9", cmake_name="armeabi-v7a with NEON"),
+    ABI("1", "armeabi",     "arm-linux-androideabi-4.9"),
     ABI("3", "arm64-v8a",   "aarch64-linux-android-4.9"),
     ABI("5", "x86_64",      "x86_64-4.9"),
-    ABI("4", "x86",         "x86-4.8"),
+    ABI("4", "x86",         "x86-4.9"),
     ABI("7", "mips64",      "mips64el-linux-android-4.9"),
-    ABI("6", "mips",        "mipsel-linux-android-4.8")
+    ABI("6", "mips",        "mipsel-linux-android-4.9")
 ]
 
 #===================================================================================================
